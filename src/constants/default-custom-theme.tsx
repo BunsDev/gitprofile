74196207720b69b3ed59bf9b73e83a37eaa92c93
Buns Enchantress <68980965+BunsDev@users.noreply.github.com>
2025-02-11 23:31:38 -0600

    Update default-custom-theme.tsx

--- src/constants/default-custom-theme.tsx
+++ src/constants/default-custom-theme.tsx
@@ -1,9 +1,9 @@
 export const DEFAULT_CUSTOM_THEME = {
-  primary: '#fc055b',
-  secondary: '#219aaf',
-  accent: '#e8d03a',
-  neutral: '#2A2730',
-  'base-100': '#E3E3ED',
+  primary: '#FEAEE0',     // pale pink
+  secondary: '#8AA6F9',   // pale blue
+  accent: '#0847F7',      // true blue
+  neutral: '#2A2730',     // blackish
+  'base-100': '#E3E3ED',  // white
   '--rounded-box': '3rem',
   '--rounded-btn': '3rem',
 };
